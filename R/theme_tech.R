e44d4d8f1b419b69b31a42e0e673a040a76adcc9
Lawrence Wu <lwu@payoff.com>
2016-08-09 14:10:11 -0700

    add payoff theme

--- R/theme_tech.R
+++ R/theme_tech.R
@@ -10,33 +10,38 @@
 #' theme_tech(theme="etsy")
 
 theme_tech <- function(theme="airbnb", tech_key = list(
-                  airbnb = list(
-                    family_title="Circular Air Bold"
-                    , family_text = "Circular Air Medium"
-                    , colour_title = "#F14000"
-                    , colour_text = "#535353"),
-                  facebook = list(
-                    family_title="Facebook Letter Faces"
-                    , family_text = "Facebook Letter Faces"
-                    , colour_title = "#3D579D"
-                    , colour_text = "#535353"),
-                  google = list(
-                    family_title="Product Sans"
-                    , family_text = "Product Sans"
-                    , colour_title = "#dd4b39"
-                    , colour_text = "black"),
-                  etsy = list(
-                    family_title="."
-                    , family_text = "."
-                    , colour_title = "#F14000"
-                    , colour_text = "#535353"),
-                  twitter = list(
-                    family_title="PicoBlackAl"
-                    , family_text = "[z] Arista Light"
-                    , colour_title = "#5380E4"
-                    , colour_text = "black")
-                    )) {
-
+  payoff = list(
+    family_title="ProximaNova-Bold"
+    , family_text = "ProximaNova-Regular"
+    , colour_title = "#002882"
+    , colour_text = "#002882"),
+  airbnb = list(
+    family_title="Circular Air Bold"
+    , family_text = "Circular Air Medium"
+    , colour_title = "#F14000"
+    , colour_text = "#535353"),
+  facebook = list(
+    family_title="Facebook Letter Faces"
+    , family_text = "Facebook Letter Faces"
+    , colour_title = "#3D579D"
+    , colour_text = "#535353"),
+  google = list(
+    family_title="Product Sans"
+    , family_text = "Product Sans"
+    , colour_title = "#dd4b39"
+    , colour_text = "black"),
+  etsy = list(
+    family_title="."
+    , family_text = "."
+    , colour_title = "#F14000"
+    , colour_text = "#535353"),
+  twitter = list(
+    family_title="PicoBlackAl"
+    , family_text = "[z] Arista Light"
+    , colour_title = "#5380E4"
+    , colour_text = "black")
+)) {
+  
   theme_classic() + 
     theme(text=element_text(size=18, family=tech_key[[theme]]$family_text)) +
     theme(legend.title=element_blank()) + 
@@ -47,14 +52,14 @@ theme_tech <- function(theme="airbnb", tech_key = list(
     theme(axis.title.x=element_text(color=tech_key[[theme]]$colour_text, vjust=0)) +
     theme(axis.title.y=element_text(color=tech_key[[theme]]$colour_text, vjust=1.25)) + 
     theme(plot.background = element_blank(),
-         panel.grid.major = element_blank(),
-         panel.grid.minor = element_blank())+
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank())+
     theme(panel.border= element_blank())+
     theme(axis.line.x = element_line(color=tech_key[[theme]]$colour_text, size = 0.5),
-        axis.line.y = element_line(color=tech_key[[theme]]$colour_text, size = 0.5)) +
-  theme(line = element_line(color=tech_key[[theme]]$colour_text)) +
-  theme(rect = element_rect(color=tech_key[[theme]]$colour_text)) +
-  theme(axis.ticks.x = element_line(color=tech_key[[theme]]$colour_text)) +
-  theme(axis.ticks.y = element_line(color=tech_key[[theme]]$colour_text))
-
+          axis.line.y = element_line(color=tech_key[[theme]]$colour_text, size = 0.5)) +
+    theme(line = element_line(color=tech_key[[theme]]$colour_text)) +
+    theme(rect = element_rect(color=tech_key[[theme]]$colour_text)) +
+    theme(axis.ticks.x = element_line(color=tech_key[[theme]]$colour_text)) +
+    theme(axis.ticks.y = element_line(color=tech_key[[theme]]$colour_text))
+  
 }
